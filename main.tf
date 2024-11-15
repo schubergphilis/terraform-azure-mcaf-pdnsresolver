6d15206d2fa0b0b437ea01f292b6c93d6932bdec
Gillian Stravers <gillianstravers@icloud.com>
2024-11-15 10:49:04 +0100

    Reworked to PDNS Resolver

--- main.tf
+++ main.tf
@@ -1,10 +1,43 @@
-resource "azure_resource_group" "rg" {
+resource "azurerm_resource_group" "this" {
   name     = var.resource_group.name
   location = var.resource_group.location
 }
 
-resource "azurerm_private_dns_zone" "pdns_zones" {
-    for_each = toset(var.dns_zones)
-    name                = each.key
-    resource_group_name = azure_resource_group.rg.name
+resource "azurerm_private_dns_resolver" "this" {
+  name                = var.private_dns_resolver.name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  virtual_network_id  = var.private_dns_resolver.virtual_network_id
 }
+
+resource "azurerm_private_dns_resolver_inbound_endpoint" "this" {
+  name                    = var.private_dns_resolver_inbound_endpoint.name
+  location                = azurerm_resource_group.this.location
+  private_dns_resolver_id = azurerm_private_dns_resolver.this.id
+
+  ip_configurations {
+    private_ip_allocation_method = var.private_dns_resolver_inbound_endpoint.private_ip_allocation_method
+    subnet_id                    = var.private_dns_resolver_inbound_endpoint.subnet_id
+    private_ip_address           = "static"
+
+  }
+}
+
+resource "azurerm_private_dns_resolver_outbound_endpoint" "this" {
+  name                    = var.private_dns_resolver_outbound_endpoint.name
+  location                = azurerm_resource_group.this.location
+  private_dns_resolver_id = azurerm_private_dns_resolver.this.id
+  subnet_id               = var.private_dns_resolver_outbound_endpoint.subnet_id
+}
+
+resource "azurerm_private_dns_resolver_forwarding_rule" "this" {
+  name                      = var.private_dns_resolver_forwarding_rule.each.name
+  dns_forwarding_ruleset_id = azurerm_private_dns_resolver.this.id
+  domain_name               = var.private_dns_resolver_forwarding_rule.each.domain_name
+  enabled                   = var.private_dns_resolver_forwarding_rule.each.enabled
+  target_dns_servers {
+    ip_address = var.private_dns_resolver_forwarding_rule.each.target_dns_servers
+    port       = var.private_dns_resolver_forwarding_rule.each.port
+  }
+}
+
--- variables.tf
+++ variables.tf
@@ -1,23 +1,42 @@
-variable "azure_resource_group" {
-    description = "Azure Resource Group"
-    type        = object({
-        name     = string
-        location = string
+variable "resource_group" {
+  description = "Resource group configuration"
+  type = object({
+    name     = string
+    location = string
+  })
 }
 
+variable "private_dns_resolver" {
+  description = "Private DNS resolver configuration"
+  type = object({
+    name               = string
+    virtual_network_id = string
+  })
+}
 
-variable "dns_zones" {
-    description = "List of DNS zones"
-    type        = list(string)
-    default     = [
-        "privatelink.blob.core.windows.net",
-        "privatelink.database.windows.net",
-        "privatelink.file.core.windows.net",
-        "privatelink.queue.core.windows.net",
-        "privatelink.redis.cache.windows.net",
-        "privatelink.servicebus.windows.net",
-        "privatelink.sql.database.windows.net",
-        "privatelink.vaultcore.azure.net",
-        "privatelink.web.core.windows.net"
-    ]
+variable "private_dns_resolver_inbound_endpoint" {
+  description = "Private DNS resolver inbound endpoint configuration"
+  type = object({
+    name                         = string
+    private_ip_allocation_method = string
+    subnet_id                    = string
+  })
+}
+
+variable "private_dns_resolver_outbound_endpoint" {
+  description = "Private DNS resolver outbound endpoint configuration"
+  type = object({
+    name      = string
+    subnet_id = string
+  })
+}
+variable "private_dns_resolver_forwarding_rule" {
+  description = "Private DNS resolver forwarding rule configuration"
+  type = map(object({
+    name               = string
+    domain_name        = string
+    enabled            = bool
+    target_dns_servers = string
+    port               = number
+  }))
 }
