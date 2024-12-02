43d0a8c5131cb3c244f4e294e7f7c434433be9ed
Gillian Stravers <gillianstravers@icloud.com>
2024-12-02 10:36:37 +0100

    fix rulesetid reference

--- main.tf
+++ main.tf
@@ -77,7 +77,7 @@ resource "azurerm_private_dns_resolver_forwarding_rule" "this" {
   for_each = var.private_dns_resolver_forwarding_rule
 
   name                      = var.private_dns_resolver_forwarding_rule[each.key].name
-  dns_forwarding_ruleset_id = azurerm_private_dns_resolver_outbound_endpoint.this[0].id
+  dns_forwarding_ruleset_id = azurerm_private_dns_resolver_dns_forwarding_ruleset.this.id
   domain_name               = var.private_dns_resolver_forwarding_rule[each.key].domain_name
   enabled                   = var.private_dns_resolver_forwarding_rule[each.key].enabled
 
@@ -94,6 +94,6 @@ resource "azurerm_private_dns_resolver_virtual_network_link" "this" {
   count = var.private_dns_resolver_outbound_endpoint.enabled ? 1 : 0
 
   name                      = "$(var.private_dns_resolver.virtual_network_name)-link"
-  dns_forwarding_ruleset_id = azurerm_private_dns_resolver_outbound_endpoint.this[count.index].id
+  dns_forwarding_ruleset_id = azurerm_private_dns_resolver_dns_forwarding_ruleset.this.id
   virtual_network_id        = var.private_dns_resolver.virtual_network_id
 }
