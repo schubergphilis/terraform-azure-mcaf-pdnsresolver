44732dc79b9100cb569d65c885e905e57070cfca
Gillian Stravers <gillianstravers@icloud.com>
2024-12-27 11:42:21 +0100

    publish branch

--- main.tf
+++ main.tf
@@ -92,3 +92,11 @@ resource "azurerm_private_dns_resolver_forwarding_rule" "this" {
     }
   }
 }
+
+resource "azurerm_private_dns_resolver_virtual_network_link" "this" {
+  count = var.private_dns_resolver_outbound_endpoint.enabled ? 1 : 0
+
+  name                      = "$(var.private_dns_resolver.virtual_network_name)-link"
+  dns_forwarding_ruleset_id = azurerm_private_dns_resolver_dns_forwarding_ruleset.this[0].id
+  virtual_network_id        = var.private_dns_resolver.virtual_network_id
+}
