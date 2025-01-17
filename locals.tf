5824e291bc2e63029234b1d138f9f253b3741462
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2025-01-17 10:23:26 +0100

    add missing locals file

---
+++ locals.tf
@@ -0,0 +1,10 @@
+locals {
+  private_dns_resolver_forwarding_rulesets = { for item in flatten([
+    for ruleset_name, ruleset_properties in var.private_dns_resolver_forwarding_rulesets : [
+      for rule in ruleset_properties.forwarding_rules : {
+        domain_name        = ruleset_properties.domain_name
+        target_dns_servers = ruleset_properties.target_dns_servers
+      }
+    ]
+  ]) : "${item.management_group}${item.role}" => item }
+}
