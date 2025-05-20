8268bb51db0b4fe683c0f1875ea505f9d365ab69
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2025-05-20 10:28:53 +0200

    set ruleset name in key

--- locals.tf
+++ locals.tf
@@ -2,9 +2,10 @@ locals {
   private_dns_resolver_forwarding_rulesets = { for item in flatten([
     for ruleset_name, ruleset_properties in var.private_dns_resolver_forwarding_rulesets : [
       for rule in ruleset_properties.forwarding_rules : {
+        ruleset_name       = ruleset_name
         domain_name        = rule.domain_name
         target_dns_servers = rule.target_dns_servers
       }
     ]
-  ]) : "${item.domain_name}" => item }
+  ]) : "${item.ruleset_name}${item.domain_name}" => item }
 }
